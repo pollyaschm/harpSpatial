34b96937ee63629b5a95eec0ff2424b9a2173b51
Polly Schmederer <polly.schmederer@geosphere.at>
2024-06-21 09:36:35 +0000

    Add Rpearson to basic scores

--- src/harpSpatial_basic_scores.cpp
+++ src/harpSpatial_basic_scores.cpp
@@ -7,12 +7,18 @@ DataFrame harpSpatial_basic_scores(NumericMatrix obfield, NumericMatrix fcfield)
 // NOTE: "abs" would give integer abs, so make sure to write std::abs
   int i,j, ni=obfield.nrow(), nj=obfield.ncol() ;
   double bias=0., mse=0., mae=0., rmse=0., tmp ;
+  double obmean, fcmean, corrnum, corrdenom1, corrdenom2, Rpearson ;
+  obmean = mean(obfield) ;
+  fcmean = mean(fcfield) ;
   for (j=0 ; j < nj ; j++) {
     for (i=0 ; i < ni ; i++) {
       tmp = fcfield(i,j) - obfield(i,j);
       bias += tmp ;
       mse  += tmp * tmp ;
       mae  += std::abs(tmp) ;
+      corrnum    += (obfield(i,j) - obmean) * (fcfield(i,j) - fcmean) ;
+      corrdenom1 += (obfield(i,j) - obmean) * (obfield(i,j) - obmean) ;
+      corrdenom2 += (fcfield(i,j) - fcmean) * (fcfield(i,j) - fcmean) ;
     }
   }
 //  Rcout << "bias: " << bias << " mse: " << mse << " mae: " << mae << std::endl;
@@ -22,13 +28,22 @@ DataFrame harpSpatial_basic_scores(NumericMatrix obfield, NumericMatrix fcfield)
   mae  /= ni*nj ;
 
   rmse  = sqrt(mse) ;
+
+  corrnum /= ni*nj ;
+  corrdenom1 /= ni*nj ;
+  corrdenom2 /= ni*nj ;
+  Rpearson = corrnum / (sqrt(corrdenom1) * sqrt(corrdenom2)) ;
   
 //  Rcout << "bias: " << bias << " mse: " << mse << " mae: " << mae << std::endl;
 //  Rcout << "rmse: " << rmse << std::endl;
+//  Rcout << "corrnum: " << corrnum << std::endl;
+//  Rcout << "corrdenom1: " << corrdenom1 << "corrdenom2: " << corrdenom2 << std::endl;
+//  Rcout << "Rpearson: " << Rpearson << std::endl;
 
   return Rcpp::DataFrame::create(Named("mse")  = mse,
                                  Named("mae")  = mae,
                                  Named("bias") = bias,
-                                 Named("rmse") = rmse);
+                                 Named("rmse") = rmse,
+				 Named("Rpearson") = Rpearson);
 
 }
