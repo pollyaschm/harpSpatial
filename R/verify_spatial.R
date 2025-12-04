241622d7e436e0666c196c84dd322b27144c119f
Polly Schmederer <polly.schmederer@geosphere.at>
2025-12-04 09:40:50 +0000

    Adapt header accordingly returned params

--- R/verify_spatial.R
+++ R/verify_spatial.R
@@ -81,7 +81,7 @@
 #'   this directory.
 #' @param sqlite_file Name of SQLite file.
 #' @param return_data If TRUE, the result is returned as a list of tables.
-#' @param return_fields If TRUE, ob_field and fc_field are added to the list of tables, return_data must set to TRUE.
+#' @param return_fields If TRUE, obfield and fcfield are added to the list of tables, return_data must set to TRUE.
 #'   To avoid data overflow, this is currently only enabled for ncases=1.
 #' @param ... Not used at thispoint (more info to be added).
 #'
